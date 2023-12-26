729e2f0f9c72dc06af0b4f269ed484af1edb9aa6
Daniel Espinosa <d111092.de@gmail.com>
2023-12-25 23:42:38 -0600

    feat: add no pokemon selected case

--- src/components/PokemonSprite.vue
+++ src/components/PokemonSprite.vue
@@ -19,15 +19,22 @@
 
 <template>
     <div @click="handleOnClick" class="pokedex-screen">
-        <img :src="pokemonSprite" :alt="pokemonName">
+        <img v-if="pokemonName" :src="pokemonSprite" :alt="pokemonName">
+        <p v-else>Choose a Pokemon!</p>
     </div>
     <p>{{ pokemonName }}</p>
 </template>
 
 <style>
     .pokedex-screen {
+        display: flex;
+        align-items: center;
         border: 1px solid black;
         width: 100px;
         height: 100px;
+    }
+
+    .pokedex-screen p {
+        text-align: center;
     }
 </style>
